3847df0b0a2876c590d656896eee9c8e1bc2c202
rrsc-dev <reis.rrsc@outlook.com>
2025-11-24 12:04:27 -0300

    definição esm e export default

---
+++ src/utils/database.js
@@ -0,0 +1,6 @@
+async function connectToDatabase(databaseName) {
+    console.log(`Conectado ao banco: ${databaseName}`);
+}
+
+// module.exports = connectToDatabase;
+export default connectToDatabase;
